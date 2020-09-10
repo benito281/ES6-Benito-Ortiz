20585447fcceeb0ae9f0bd9830e5948910e188ef
benito281 <62038704+benito281@users.noreply.github.com>
2020-09-10 18:33:29 -0300

    Trabajo completado

--- act1.js
+++ act1.js
@@ -1,3 +1,11 @@
-function hola(){
-    console.log("holas");
+let saludo = (persona) => 
+{ return '¿hola como estas '+persona+'?'};
+
+console.log(saludo('Willy'));
+
+const alumno = (nombre,apellido,carrera,edad) =>{
+    let datos={nombre:nombre,apellido:apellido,carrera:carrera,edad:edad}
+    return datos;
 }
+console.log(alumno('pepe','perez','mecatronica',25));
+
